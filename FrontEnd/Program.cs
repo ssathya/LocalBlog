23289c0302413b8dd7b687fc810555dd2d232924
Sridhar <sridhar_0314@live.com>
2024-12-30 11:46:46 -0500

    Add project files.

---
+++ FrontEnd/Program.cs
@@ -0,0 +1,60 @@
+using Auth0.AspNetCore.Authentication;
+using FrontEnd.AuthenticationStateSyncer;
+using FrontEnd.Components;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components.Authorization;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
+
+//Auth0
+builder.Services
+    .AddAuth0WebAppAuthentication(options =>
+    {
+        options.Domain = builder.Configuration["Auth0:Domain"] ?? "Domain not populated";
+        options.ClientId = builder.Configuration["Auth0:ClientId"] ?? "Client Id not populated";
+    });
+//Auth0
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+//Auth0
+app.MapGet("/Account/Login", async (HttpContext context, string returnUrl = "/") =>
+{
+    var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+        .WithRedirectUri(returnUrl)
+        .Build();
+    await context.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+});
+app.MapGet("/Account/Logout", async (HttpContext context) =>
+{
+    var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
+         .WithRedirectUri("/")
+         .Build();
+    await context.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+    await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+});
+//Auth0
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ Readme.md
@@ -0,0 +1,1 @@
+Blog Engine in Blazor
