b4ab07210b74f1582de2d0d65cde4bd5317d19f0
Sridhar <sridhar_0314@live.com>
2025-01-16 17:09:27 -0500

    Fixes LB-1-Chang the size of introduction and Title
    - Updated Title's max lenght from 120 to 255
    - Slug goes from 125 to 255
    - Inrtoduction is upaded from 255 to 2047

---
+++ Models/Migrations/20250116212601_ChangeFieldSizes.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Models.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeFieldSizes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "character varying(125)",
+                maxLength: 125,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "BlogPosts",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(120)",
+                oldMaxLength: 120);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Slug",
+                table: "BlogPosts",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(125)",
+                oldMaxLength: 125);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Introduction",
+                table: "BlogPosts",
+                type: "character varying(2047)",
+                maxLength: 2047,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(125)",
+                oldMaxLength: 125);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "BlogPosts",
+                type: "character varying(120)",
+                maxLength: 120,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Slug",
+                table: "BlogPosts",
+                type: "character varying(125)",
+                maxLength: 125,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Introduction",
+                table: "BlogPosts",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(2047)",
+                oldMaxLength: 2047);
+        }
+    }
+}
